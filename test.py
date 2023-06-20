18da4f014c95a51482f239a3ac22160e630efab8
Krishnan Prashanth <Krishnan.Prashanth@HPE.com>
2023-06-20 14:08:16 -0700

    GLCP-3152: Everything Starts Somewhere

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
-print("This is a test file, which I can call from github actions")
+
+f = open("temp.out", "w")
+
+print("This is a test file, which I can call from github actions", file=f)
 
 from math import pi, exp
-print(f"PI = {pi:5f}, e^pi = {exp(pi):10f}")
+print(f"PI = {pi:5f}, e^pi = {exp(pi):10f}", file = f)
 
 import os
 
-print(os.listdir("."))
+print(os.listdir("."), file=f)
+
+f.close()
